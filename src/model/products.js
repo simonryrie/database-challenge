2f8e34daa3b1923f2e5bd3d0e808e15d62e56977
simonryrie <simonryrie@gmail.com>
2023-05-19 21:39:12 +0100

    Initial commit - first challenge complete but untested

--- src/model/products.js
+++ src/model/products.js
@@ -1,1 +1,18 @@
 const db = require("../database/db.js");
+
+const select_products = db.prepare(/*sql*/ `
+  SELECT
+    id,
+    name,
+    quantity_per_unit,
+    unit_price,
+    units_in_stock,
+    units_on_order
+  FROM products
+`);
+
+function listProducts() {
+  return select_products.all();
+}
+
+module.exports = { listProducts };
